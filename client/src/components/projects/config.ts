cbad192cb53c1d085375d8711b133defa7038079
Field Palmer <gfieldpalmer@gmail.com>
2023-09-25 16:39:52 -0600

    added projects

--- client/src/components/projects/config.ts
+++ client/src/components/projects/config.ts
@@ -1,81 +1,87 @@
-import { getSkill, Skill } from "../skills/config"
-import Portfolio from "./../../images/portfolio-sg.png";
-import LandingPage from "./../../images/pwa-sg.png";
-import Shazamify from "./../../images/shazamify-sg.png";
-import Dashboard from "./../../images/dashboard-sg.png";
+import { getSkill, Skill } from '../skills/config';
+import Portfolio from './../../images/portfolio-sg.png';
+import LandingPage from './../../images/pwa-sg.png';
+import Shazamify from './../../images/shazamify-sg.png';
+import Dashboard from './../../images/dashboard-sg.png';
+import mevArt from './../../images/mevArt.png';
+import thriveSampson from './../../images/thriveSampson.png';
 
 export type Project = {
-  name: string,
-  description: string,
-  image: any,
-  url: string,
-  github: string,
-  technologies: Skill[],
-}
+   name: string;
+   description: string;
+   image: any;
+   url: string;
+   github: string;
+   technologies: Skill[];
+};
 
 export const projects: Project[] = [
-  {
-    name: "Portfolio",
-    description: "Developer portfolio showing my blockchain projects, skillset and certificates.",
-    image: Portfolio,
-    url: "https://tijan.dev/",
-    github: "https://github.com/0xTijan/portfolio",
-    technologies: getSkill(["react", "typescript", "html", "css", "git", "npm"])
-  },
-  {
-    name: "Data Dashboard",
-    description: "React and Syncfusion allow us to fluidly display data visualizations.",
-    image: Dashboard,
-    url: "https://datadashdemo.web.app/",
-    github: "https://github.com/fieldpalmer/data-dashboard",
-    technologies: getSkill(["react", "tailwind", "javascript", "firebase", "git", "npm"])
-  },
-  {
-    name: "Music Streaming App",
-    description: "Users can stream free clips of thousands of songs thanks to ShazamCore API.",
-    image: Shazamify,
-    url: "https://shazamify.netlify.app/",
-    github: "https://github.com/fieldpalmer/social-music-app",
-    technologies: getSkill(["react", "javascript", "netlify", "tailwind", "git", "npm"])
-  },
-  {
-    name: "Business Landing Page",
-    description: "A fully responsive web app built with React and Tailwind to highlight a business or product.",
-    image: LandingPage,
-    url: "https://pwawebapp.netlify.app/",
-    github: "https://github.com/fieldpalmer/responsive-web-app",
-    technologies: getSkill(["react", "tailwind", "nodejs", "netlify"])
-  },
-  // {
-  //   name: "AWS Amplify",
-  //   description: "Shows on-chain stats off user's address.",
-  //   image: Portfolio,
-  //   url: "https://github.com/0xTijan/mobile-wallet",
-  //   github: "https://github.com/0xTijan/mobile-wallet",
-  //   technologies: getSkill(["react native", "javascript", "moralis", "css", "npm"])
-  // },
-  // {
-  //   name: "Dating App",
-  //   description: "Multiplayer Coin Flip game using smart contracts and chainlink VRF to determine a winner in decentralized way.",
-  //   image: Portfolio,
-  //   url: "https://projects.tijan.dev/coin-flip",
-  //   github: "https://github.com/0xTijan/coin-flip",
-  //   technologies: getSkill(["react", "solidity", "hardhat", "typescript", "chainlink", "moralis"])
-  // },
-  // {
-  //   name: "Online Checkout",
-  //   description: "Funny blockchain game that prevents you to panic sell your cryptos by locking them in smart contract.",
-  //   image: Portfolio,
-  //   url: "https://projects.tijan.dev/diamond-hands",
-  //   github: "https://github.com/0xTijan/diamond-hands",
-  //   technologies: getSkill(["solidity", "hardhat", "react", "javascript", "moralis"])
-  // },
-  // {
-  //   name: "Social Network",
-  //   description: "Shows on-chain stats off user's address. User can add personal contacts for easy assets transfer.",
-  //   image: Portfolio,
-  //   url: "https://github.com/0xTijan/mobile-wallet",
-  //   github: "https://github.com/0xTijan/mobile-wallet",
-  //   technologies: getSkill(["react native", "javascript", "moralis", "css", "npm"])
-  // },
-]
+   {
+      name: 'Portfolio',
+      description:
+         'Developer portfolio showing my blockchain projects, skillset and certificates.',
+      image: Portfolio,
+      url: 'https://tijan.dev/',
+      github: 'https://github.com/0xTijan/portfolio',
+      technologies: getSkill(['react', 'typescript', 'html', 'css', 'git', 'npm'])
+   },
+   {
+      name: 'Data Dashboard',
+      description:
+         'React and Syncfusion allow us to fluidly display data visualizations.',
+      image: Dashboard,
+      url: 'https://datadashdemo.web.app/',
+      github: 'https://github.com/fieldpalmer/data-dashboard',
+      technologies: getSkill([
+         'react',
+         'tailwind',
+         'javascript',
+         'firebase',
+         'git',
+         'npm'
+      ])
+   },
+   {
+      name: 'Music Streaming App',
+      description:
+         'Users can stream free clips of thousands of songs thanks to ShazamCore API.',
+      image: Shazamify,
+      url: 'https://shazamify.netlify.app/',
+      github: 'https://github.com/fieldpalmer/social-music-app',
+      technologies: getSkill([
+         'react',
+         'javascript',
+         'netlify',
+         'tailwind',
+         'git',
+         'npm'
+      ])
+   },
+   {
+      name: 'Business Landing Page',
+      description:
+         'A fully responsive web app built with React and Tailwind to highlight a business or product.',
+      image: LandingPage,
+      url: 'https://pwawebapp.netlify.app/',
+      github: 'https://github.com/fieldpalmer/responsive-web-app',
+      technologies: getSkill(['react', 'tailwind', 'nodejs', 'netlify'])
+   },
+   {
+      name: 'e-Commerce Site for Local Artist',
+      description:
+         'An online art gallery built with Squarespace and integrated with Google Analytics and Ads.',
+      image: mevArt,
+      url: 'https://markviser.com',
+      github: '',
+      technologies: getSkill(['web design', 'google analytics', 'google ads'])
+   },
+   {
+      name: 'Local Physical Therapy Clinic Landing Page',
+      description:
+         'An information portal uilt with Squarespace and integrated with Google Ads, Analytics, and Calendar',
+      image: thriveSampson,
+      url: 'https://thrivesampson.com',
+      github: '',
+      technologies: getSkill(['web design', 'google analytics', 'google ads'])
+   }
+];
