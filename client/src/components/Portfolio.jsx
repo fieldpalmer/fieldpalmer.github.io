3665af29712bd0a23a84ebe74e9063fe0c04875b
Field Palmer <gfieldpalmer@gmail.com>
2024-05-10 16:31:06 -0600

    portfolio modals

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import data_and_dbs from '../assets/data_and_dbs.png';
 import design_dev from '../assets/design_dev.png';
 import systems from '../assets/systems.png';
@@ -25,13 +26,37 @@ const images = [
 ];
 
 export default function Portfolio() {
+   const [show, setShow] = useState(false);
+   const handleClose = () => setShow(false);
+   const handleShow = () => setShow(true);
+
    return (
       <div className='portfolio-content text-center my-2'>
          <div className='row'>
+            <Modal show={show} onHide={handleClose} centered>
+               <Modal.Header closeButton>
+                  <Modal.Title>
+                     <code>Projects</code>
+                  </Modal.Title>
+               </Modal.Header>
+               <Modal.Body>
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+                     illum consequatur voluptates eaque nam accusantium maxime neque
+                     eligendi voluptate. Ipsam, delectus veritatis voluptate illum
+                     repellat inventore unde ipsum cumque iusto.
+                  </p>
+               </Modal.Body>
+               <Modal.Footer closeButton>
+                  <Button variant='secondary' onClick={handleClose}>
+                     Close
+                  </Button>
+               </Modal.Footer>
+            </Modal>
             {images.map((image, i) => (
                <div key={i} className='col-md-4 col-12'>
                   <div className='circle-image mx-3'>
-                     <img src={image.src} alt={image.alt} />
+                     <img src={image.src} alt={image.alt} onClick={handleShow} />
                      <p class='circle-text'>Design & Development</p>
                   </div>
                </div>
