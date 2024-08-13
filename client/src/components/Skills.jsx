72eea9b1650d148f3a1138d5c62034ae51c217df
Field Palmer <gfieldpalmer@gmail.com>
2024-08-13 00:11:03 -0600

    skills patchworked together

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import { FaReact, FaNodeJs, FaPython } from 'react-icons/fa';
+import { Row, Col } from 'react-bootstrap';
+import {
+   FaReact,
+   FaNodeJs,
+   FaPython,
+   FaGithub,
+   FaAngular,
+   FaRust
+} from 'react-icons/fa';
+import { IoLogoFirebase } from 'react-icons/io5';
+import {
+   SiGraphql,
+   SiMongodb,
+   SiOpenai,
+   SiGoogleappsscript,
+   SiSolidity
+} from 'react-icons/si';
+
 export default function Skills() {
    return (
-      <h5>
-         <ListGroup horizontal>
-            <ListGroup.Item>
-               <FaReact />
-            </ListGroup.Item>
-            <ListGroup.Item>
-               <FaNodeJs />
-            </ListGroup.Item>
-            <ListGroup.Item>
-               <FaPython />
-            </ListGroup.Item>
-            <ListGroup.Item>horizontally!</ListGroup.Item>
-         </ListGroup>
-      </h5>
+      <div>
+         <Row>
+            <Col>
+               <FaReact className='skill-icon' />
+               <FaNodeJs className='skill-icon' />
+               <FaPython className='skill-icon' />
+               <IoLogoFirebase className='skill-icon' />
+               <SiGoogleappsscript className='skill-icon' />
+               <FaAngular className='skill-icon' />
+               <SiSolidity className='skill-icon d-md-none' />
+               <FaRust className='skill-icon d-md-none' />
+            </Col>
+         </Row>
+         <Row>
+            <Col>
+               <FaGithub className='skill-icon' />
+               <SiGraphql className='skill-icon' />
+               <SiMongodb className='skill-icon' />
+               <SiOpenai className='skill-icon' />
+               <SiSolidity className='skill-icon d-none d-md-inline' />
+               <FaRust className='skill-icon d-none d-md-inline' />
+            </Col>
+         </Row>
+      </div>
    );
 }
