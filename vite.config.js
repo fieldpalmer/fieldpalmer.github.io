dbe1682a696337f6f9743e7bb15d3fb6f5035897
Field Palmer <gfieldpalmer@gmail.com>
2025-03-02 18:13:51 -0700

    config reconfigured for personal site hosting"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/fieldpalmer.github.io',
-   plugins: [react()]
+   plugins: [react()],
+   base: '/',
+   build: {
+      outDir: 'dist',
+      emptyOutDir: true
+   }
 });
