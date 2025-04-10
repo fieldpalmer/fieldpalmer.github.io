a281b076cf9dcc5c2722e6ea02acb1b3c36f874b
Field Palmer <gfieldpalmer@gmail.com>
2025-04-10 17:23:33 -0600

    background color full bleed

--- src/components/Front/Front-Side.jsx
+++ src/components/Front/Front-Side.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import profPic from '../../assets/images/profPic.png';
 import IconsList from '../IconsList';
 
-export default function FrontSide() {
-   return (
-      <>
-         <div className='circle-image'>
-            <img alt='profPic' src={profPic} />
-         </div>
-         <div>
-            <h1>FIELD PALMER</h1>
-            <hr />
-            <small>Full-Stack Developer + Solutions Engineer</small>
-            <hr />
-            <IconsList />
-         </div>
-      </>
-   );
-}
+const FrontSide = () => {
+     return (
+          <>
+               <div className='circle-image'>
+                    <img alt='profPic' src={profPic} />
+               </div>
+               <div>
+                    <h1>FIELD PALMER</h1>
+                    <hr />
+                    <small>Full-Stack Developer + Automation Engineer</small>
+                    <hr />
+                    <IconsList />
+               </div>
+          </>
+     );
+};
+
+export default FrontSide;
